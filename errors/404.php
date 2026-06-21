bebbd47fd0ac822f645a33b4e2fa183bacb87b4c
sfazag <gianghuwowng@gmail.com>
2026-06-21 23:25:48 +0700

    Delete errors directory
    Signed-off-by: sfazag <gianghuwowng@gmail.com>

--- errors/404.php
+++
@@ -1,9 +0,0 @@
-<?php require_once __DIR__ . '/../layout/header.php'; ?>
-<div class="text-center py-5">
-    <div class="display-1 fw-bold text-muted">404</div>
-    <h4 class="mb-3">Trang không tìm thấy</h4>
-    <a href="<?= APP_URL ?>/index.php?page=dashboard" class="btn btn-primary">
-        <i class="bi bi-house me-2"></i>Về trang chủ
-    </a>
-</div>
-<?php require_once __DIR__ . '/../layout/footer.php'; ?>
